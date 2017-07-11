971f103b8586ae6476f0e29ecd6e7fd840044389
Shayla303 <shaylarichard@hotmail.com>
2017-07-11 17:10:21 -0600

    Add files via upload
    Updated with new test for reverse string array and instantiator tests both passed

--- array-test.js
+++ array-test.js
@@ -6,13 +6,13 @@ describe('iterator', function() {
   it('can be instantiated', function() {
     var factory = new Iterator();
 
-    assert.equal(typeof factory, Iterator);
+    assert.equal(typeof factory, 'object');
   });
 
   it('has a prototype function called uppercase', function() {
     var factory = new Iterator();
 
-    assert.equal(typeof factory.createArray, 'function');
+    assert.equal(typeof factory.uppercase, 'function');
   });
 
   it('using FOR EACH can create a string from an array of strings', function() {
@@ -23,15 +23,15 @@ describe('iterator', function() {
     assert.equal(factory.createArray([10, 20, 30, 40, 50]), 150);
   });
 
-  it.skip('can create a string of YELLING UPPER CASE words using FOR EACH', function() {
+  it('can create a string of YELLING UPPER CASE words using FOR EACH', function() {
     var factory = new Iterator();
 
     assert.equal(factory.yell(["one, ", "two, ", "three"]), "ONE! TWO! THREE!");
 
-    assert.equal(factory.yell(["hey, ", "what's ", "up"]), "HEY! WHATS! UP!");
+    assert.equal(factory.yell(["hey, ", "what's, ", "up"]), "HEY! WHAT'S! UP!");
   });
 
-  it.skip('using FOR EACH can reverse an array and create a string', function() {
+  it('using FOR EACH can reverse an array and create a string', function() {
     var factory = new Iterator();
 
     assert.equal(factory.reverseAndString([1, 2, 3, 4, 5]), '5 4 3 2 1');
@@ -39,7 +39,7 @@ describe('iterator', function() {
     assert.equal(factory.reverseAndString([10, 11, 12, 13, 14]), '14 13 12 11 10');
   })
 
-  it.skip('using FOR EACH can sum a bunch of numbers', function() {
+  it('using FOR EACH can sum a bunch of numbers', function() {
     var factory = new Iterator();
 
     assert.equal(factory.sum([1, 2, 3, 4, 5, 6]), 21);
@@ -47,7 +47,7 @@ describe('iterator', function() {
     assert.equal(factory.sum([7, 8, 9, 10, 11, 12]), 57);
   });
 
-  it.skip('using FOR EACH can square every number in a collection and then sum them', function() {
+  it('using FOR EACH can square every number in a collection and then sum them', function() {
     var factory = new Iterator();
 
     assert.equal(factory.squareAndSum([1, 2, 3, 4, 5, 6]), 91);
@@ -64,41 +64,41 @@ describe('iterator', function() {
   it.skip('can FILTER out odd numbers from a collection of integers', function() {
     var factory = new Iterator();
 
-    assert.equal(factory.onlyEvens([1, 2, 3, 4, 5, 6]), [2, 4, 6]);
+    assert.deepEqual(factory.onlyEvens([1, 2, 3, 4, 5, 6]), [2, 4, 6]);
 
-    assert.equal(factory.onlyEvens([7, 8, 9, 10, 11, 12]), [8, 10, 12]);
+    assert.deepEqual(factory.onlyEvens([7, 8, 9, 10, 11, 12]), [8, 10, 12]);
   });
 
   it.skip('can FILTER out even numbers from a collection of integers', function() {
     var factory = new Iterator();
 
-    assert.equal(factory.onlyOdds([1, 2, 3, 4, 5, 6]), [1, 3, 5]);
+    assert.deepEqual(factory.onlyOdds([1, 2, 3, 4, 5, 6]), [1, 3, 5]);
 
-    assert.equal(factory.onlyOdds([7, 8, 9, 10, 11, 12]), [7, 9, 11]);
+    assert.deepEqual(factory.onlyOdds([7, 8, 9, 10, 11, 12]), [7, 9, 11]);
   });
 
   it.skip('can FILTER out strings shorter or equal to 4 letters', function() {
     var factory = new Iterator();
 
-    assert.equal(factory.shortWords(['potato', 'bird', 'ham', 'lasagna', 'pants', 'bro', 'consternation']), ['bird', 'ham', 'bro']);
+    assert.deepEqual(factory.shortWords(['potato', 'bird', 'ham', 'lasagna', 'pants', 'bro', 'consternation']), ['bird', 'ham', 'bro']);
   });
 
   it.skip('can return a new array of uppercase strings using MAP', function() {
     var factory = new Iterator();
 
-    assert.equal(factory.uppercaseArray(['potato', 'bird', 'ham', 'pants']), ['POTATO', 'BIRD', 'HAM', 'PANTS']);
+    assert.deepEqual(factory.uppercaseArray(['potato', 'bird', 'ham', 'pants']), ['POTATO', 'BIRD', 'HAM', 'PANTS']);
   });
 
   it.skip('can return a new array of integers that are increased by 10 using MAP', function() {
     var factory = new Iterator();
 
-    assert.equal(factory.addTen([1, 2, 3, 4, 5]), [11, 12, 13, 14, 15]);
+    assert.deepEqual(factory.addTen([1, 2, 3, 4, 5]), [11, 12, 13, 14, 15]);
   });
 
   it.skip('can return an array of data types based on each element', function() {
     var factory = new Iterator();
 
-    assert.equal(factory.dataTypes(['potato', 1, true, 'lasagna', 444, null]), ["string", "number", "boolean", "string", "number", "object"]);
+    assert.deepEqual(factory.dataTypes(['potato', 1, true, 'lasagna', 444, null]), ["string", "number", "boolean", "string", "number", "object"]);
   });
 
   it.skip('can FILTER out strings shorter or equal to 4 letters', function() {
@@ -111,37 +111,40 @@ describe('iterator', function() {
     var factory = new Iterator();
 
     var meals = [
-    { name: 'Lasagna', vegetarian: true, ingredients: ['tomatoes', 'basil', 'cheese'] },
-    { name: 'Pizza', vegetarian: true, ingredients: ['basil', 'tomatoes', 'cheese'] },
-    { name: 'Goulash', vegetarian: false, ingredients: ['tomatoes', 'meat'] },
-    { name: 'Bean and Rice', vegetarian: true, ingredients: ['beans', 'rice'] },
+      { name: 'Lasagna', vegetarian: true, ingredients: ['tomatoes', 'basil', 'cheese'] },
+      { name: 'Pizza', vegetarian: true, ingredients: ['basil', 'tomatoes', 'cheese'] },
+      { name: 'Goulash', vegetarian: false, ingredients: ['tomatoes', 'meat'] },
+      { name: 'Bean and Rice', vegetarian: true, ingredients: ['beans', 'rice'] },
     ];
 
     var search = 'basil'
 
-    var expected = { name: 'Pizza', vegetarian: true, ingredients: ['basil', 'tomatoes', 'cheese'] }
+    var expected = [
+      { name: 'Lasagna', vegetarian: true, ingredients: ['tomatoes', 'basil', 'cheese'] },
+      { name: 'Pizza', vegetarian: true, ingredients: ['basil', 'tomatoes', 'cheese'] }
+    ]
 
-    assert.equal(factory.getFood(meals, search), expected)
+    assert.deepEqual(factory.getFood(meals, search), expected)
   });
 
   it.skip('can SORT an array of numbers', function() {
     var factory = new Iterator();
 
-    assert.equal(factory.sorted([5, 2, 6, 3, 1]), [1, 2, 3, 5, 6]);
-    assert.equal(factory.sorted([8, 10, 12, 7, 3]), [3, 7, 8, 10, 12]);
+    assert.deepEqual(factory.sorted([5, 2, 6, 3, 1]), [1, 2, 3, 5, 6]);
+    assert.deepEqual(factory.sorted([8, 10, 12, 7, 3]), [3, 7, 8, 10, 12]);
   });
 
   it.skip('can SORT an array of numbers in reverse order', function() {
     var factory = new Iterator();
 
-    assert.equal(factory.sorted([5, 2, 6, 3, 1]), [6, 5, 3, 2, 1]);
-    assert.equal(factory.sorted([8, 10, 12, 7, 3]), [12, 10, 8, 7, 3]);
+    assert.deepEqual(factory.sorted2([5, 2, 6, 3, 1]), [6, 5, 3, 2, 1]);
+    assert.deepEqual(factory.sorted2([8, 10, 12, 7, 3]), [12, 10, 8, 7, 3]);
   });
 
   it.skip('can SORT an array of strings', function() {
     var factory = new Iterator();
 
-    assert.equal(factory.sorted(['zoo', 'apple', 'zucchini', 'potato', 'beets']), ['apple', 'beets', 'potato', 'zoo', 'zucchini']);
+    assert.deepEqual(factory.sorted(['zoo', 'apple', 'zucchini', 'potato', 'beets']), ['apple', 'beets', 'potato', 'zoo', 'zucchini']);
   });
 
   it.skip('can MAP over a SORTED array returning meals in order of most ingredients ', function() {
@@ -156,7 +159,7 @@ describe('iterator', function() {
 
     var expected = ['Pizza', 'Lasagna', 'Goulash', 'Beans & Rice']
 
-    assert.equal(factory.sortByIngredients(meals), expected);
+    assert.deepEqual(factory.sortByIngredients(meals), expected);
   });
 
 });
--- arrays.js
+++ arrays.js
@@ -1,20 +1,69 @@
-function Iterator () {
+function Iterator(input) {}
 
+Iterator.prototype.uppercase = function() {}
+
+Iterator.prototype.reverseAndString = function(arr) {
+  let answer = '';
+  const reversedArray = arr.reverse();
+  reversedArray.forEach(function (char, i){
+    if (i === arr.length - 1) {
+      answer += char
+    } else {
+      answer += char + ' ';
+    }
+  })
+  return answer;
 }
 
-iterator.prototype.createArray = function () {
+Iterator.prototype.createArray = function(arr) {
 
+  if (typeof arr[0] === 'number' ) {
+    var total = 0
+    arr.forEach(function(num){
+      total += num;
+    })
+    return total
+  } else if (typeof arr[0] === 'string') {
+    var text = ''
+    arr.forEach(function(word, i){
+      if (i === arr.length - 1) {
+        text += word
+      } else {
+        text += word + ' ';
+      }
+    })
+    return text
+  }
 }
 
-iterator.prototype.createArray = function (){
-  var myArray = ['hello', 'world', 'happy', 'monday'];
-  myArray.forEach(function(Array){
-    return myArray.toString();
-  });
+Iterator.prototype.yell = function(words) {
+
+  var text = ''
+  words.forEach(function(word, i){
+    if (i === words.length - 1) {
+    text += word.toUpperCase() + '!';
+  } else {
+    text += word.substring(0, word.length - 2 ).toUpperCase() + '! '
+  }
+  })
+  return text
 }
 
+Iterator.prototype.sum = function(arr){
+  var total = 0;
+  arr.forEach(function(num){
+    total += num;
+  })
+  return total
+}
 
-
+Iterator.prototype.squareAndSum = function(arr){
+  var total = 0;
+  arr.forEach(function(num){
+    total += num * num;
+  })
+  return total
+}
 
 
 module.exports = Iterator;
